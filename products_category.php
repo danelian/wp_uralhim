528b048488faa87978289f6c6f362c59eef5ba2d
danelian <141317269+danelian@users.noreply.github.com>
2024-03-01 00:40:17 +0400

    products_category: upobl, ctabl

--- products_category.php
+++ products_category.php
@@ -120,8 +120,35 @@ get_header(); ?>
   </div>
 </section>
 
-<?php get_template_part('template-parts/upobl'); ?>
+<?php if (get_field('upobl_text_category', 'options')) { ?>
+  <section class="upobl">
+    <div class="container">
+      <div class="upobl__content">
+        <h2><?php the_field('upobl_text_category', 'options'); ?></h2>
+      </div>
+    </div>
+  </section>
+<?php } ?>
 
-<?php get_template_part('template-parts/ctabl'); ?>
+<section class="ctabl" id="cta">
+  <div class="container">
+    <div class="default__container">
+      <div class="default__container-left">
+        <?php if (get_field('ctabl_section_category', 'options')) { ?><h2 class="default-title"><?php the_field('ctabl_section_category', 'options'); ?></h2><?php } ?>
+      </div>
+      <div class="default__container-right">
+        <div class="ctabl__form">
+          <?php if (get_field('ctabl_title_category', 'options')) { ?><h2 class="section-title"><?php the_field('ctabl_title_category', 'options'); ?></h2><?php } ?>
+          <?php if (get_field('ctabl_text_category', 'options')) { ?><p><?php the_field('ctabl_text_category', 'options'); ?></p><?php } ?>
+          <?php echo do_shortcode('[contact-form-7 id="cfa5555" title="Контактная форма"]'); ?>
+          <div class="ctabl__socials">
+            <h3>Или свяжитесь с нами самостоятельно прямо сейчас!</h3>
+            <?php get_template_part('template-parts/socials'); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
